a2a089b090b550e172cee8e9a791c417f1bf7b05
unknown <gdutt2304@gmail.com>
2024-12-16 12:20:24 +0530

    Update Operators

---
+++ src/class4.java
@@ -0,0 +1,52 @@
+public class class4 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+//        int a = 10;
+//        int A = 56;
+//        int $ = 10;
+//        String str12 = "Ganesh dutt";
+
+
+//        int a = 10;
+//        int b = 3;
+//        float c = a / b;
+//        System.out.println(c);
+
+
+//        int a = 10;
+//        int b = 10;
+//        System.out.println(a >= b);
+
+//        boolean value1 = true;
+//        boolean value2 = false;
+//        System.out.println(!value1);
+
+
+//        int a = 10;
+//         a = a+5;
+
+//         int b = 20;
+//         b +=5;  //b = b+5
+//
+//        System.out.println(a);
+//        System.out.println(b);int b = 20;
+//         b +=5;  //b = b+5
+//
+//        System.out.println(a);
+//        System.out.println(b);
+
+        int a = 80;
+        int b = 20;
+
+        String c = (a > b)? "a is greater then b" : "b is greater then a";
+        System.out.println(c);
+//        a++; //11
+//        a--;  // 10
+//        System.out.println(a); a++; //11
+//        a--;  // 10
+//        System.out.println(a);
+
+
+
+    }
+}
