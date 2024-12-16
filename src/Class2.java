879e87ac9e38ac8cd2675164cbfb4a2086b94748
unknown <gdutt2304@gmail.com>
2024-12-16 11:50:11 +0530

    first commit

---
+++ src/Class2.java
@@ -0,0 +1,13 @@
+public class Class2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int a =123456;
+        String str = "Number ";
+//        int b = 23;
+//        int c  = a + b;
+        System.out.println(str + a);
+        System.out.println("I am number : " + a);
+
+    }
+}
---
+++ src/class3.java
@@ -0,0 +1,21 @@
+public class class3 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        byte a = 10;
+        short b = 20;
+//        int c = a + b;
+        long  d = 34439;
+
+        float e1 = 3.5f;
+        double e2 = 45.392832398d;
+
+        char ch = 'A';
+//        String str = "Ayush";
+boolean b1 = true;
+        boolean b2 = false;
+
+
+
+
+    }
+}
