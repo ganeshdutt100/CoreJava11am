a95902a0dda33df3eb5d467f6220301ba0af49c4
unknown <gdutt2304@gmail.com>
2024-12-17 11:18:29 +0530

    Update Conditions statments

---
+++ src/class5.java
@@ -0,0 +1,37 @@
+public class class5 {
+    public static void main(String[] args) {
+//        int a = 10;
+//        if(a == 5){
+//            System.out.println(true);
+//        }
+//        else{
+//            System.out.println(false);
+//        }
+
+        int weekDays  = 5;
+        if(weekDays == 1){
+            System.out.println("Monday");
+        }
+        else if (weekDays == 2){
+            System.out.println("Tuesday");
+        }
+        else if (weekDays == 3){
+            System.out.println("Wednesday");
+        }
+        else if (weekDays == 4){
+            System.out.println("Thursday");
+        }
+        else if (weekDays == 5){
+            System.out.println("Friday");
+        }
+        else if (weekDays == 6){
+            System.out.println("Saturday");
+        }
+        else if (weekDays == 7){
+            System.out.println("Sunday");
+        }
+        else{
+            System.out.println("Invalid weekday");
+        }
+    }
+}
