6fce843d15eed49f98cf2bc23df7463fa902e4a2
unknown <gdutt2304@gmail.com>
2024-12-17 11:31:29 +0530

    Nested If

--- src/class5.java
+++ src/class5.java
@@ -8,30 +8,46 @@ public class class5 {
 //            System.out.println(false);
 //        }
 
-        int weekDays  = 5;
-        if(weekDays == 1){
-            System.out.println("Monday");
-        }
-        else if (weekDays == 2){
-            System.out.println("Tuesday");
-        }
-        else if (weekDays == 3){
-            System.out.println("Wednesday");
-        }
-        else if (weekDays == 4){
-            System.out.println("Thursday");
-        }
-        else if (weekDays == 5){
-            System.out.println("Friday");
-        }
-        else if (weekDays == 6){
-            System.out.println("Saturday");
-        }
-        else if (weekDays == 7){
-            System.out.println("Sunday");
+//        int weekDays  = 5;
+//        if(weekDays == 1){
+//            System.out.println("Monday");
+//        }
+//        else if (weekDays == 2){
+//            System.out.println("Tuesday");
+//        }
+//        else if (weekDays == 3){
+//            System.out.println("Wednesday");
+//        }
+//        else if (weekDays == 4){
+//            System.out.println("Thursday");
+//        }
+//        else if (weekDays == 5){
+//            System.out.println("Friday");
+//        }
+//        else if (weekDays == 6){
+//            System.out.println("Saturday");
+//        }
+//        else if (weekDays == 7){
+//            System.out.println("Sunday");
+//        }
+//        else{
+//            System.out.println("Invalid weekday");
+//        }
+
+
+        int Recharge = 100;
+        int P_YouTube = 300;
+        if(Recharge >= 200){
+            System.out.println("You can watch YouTube");
+            if(P_YouTube >= 300){
+                System.out.println("You can watch YouTube without ADDS");
+            }
+            else{
+                System.out.println("You can watch YouTube with ADDS");
+            }
         }
         else{
-            System.out.println("Invalid weekday");
+            System.out.println("You can't watch YouTube");
         }
     }
 }
