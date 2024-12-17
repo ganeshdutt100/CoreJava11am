fa5f55478851f455cdef3050ddd95173a49482ef
unknown <gdutt2304@gmail.com>
2024-12-17 11:49:27 +0530

    Update Switch

--- src/class5.java
+++ src/class5.java
@@ -35,19 +35,53 @@ public class class5 {
 //        }
 
 
-        int Recharge = 100;
-        int P_YouTube = 300;
-        if(Recharge >= 200){
-            System.out.println("You can watch YouTube");
-            if(P_YouTube >= 300){
-                System.out.println("You can watch YouTube without ADDS");
-            }
-            else{
-                System.out.println("You can watch YouTube with ADDS");
-            }
-        }
-        else{
-            System.out.println("You can't watch YouTube");
+//        int Recharge = 100;
+//        int P_YouTube = 300;
+//        if(Recharge >= 200){
+//            System.out.println("You can watch YouTube");
+//            if(P_YouTube >= 300){
+//                System.out.println("You can watch YouTube without ADDS");
+//            }
+//            else{
+//                System.out.println("You can watch YouTube with ADDS");
+//            }
+//        }
+//        else{
+//            System.out.println("You can't watch YouTube");
+//        }
+
+        int weekDay = 6;
+        switch (weekDay){
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                    System.out.println("Thursday");
+                    break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("Invalid weekday");
         }
     }
 }
