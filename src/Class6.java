28581078161d50b8f8fc023cf4d7edc6ffe694b5
unknown <gdutt2304@gmail.com>
2024-12-18 11:54:45 +0530

    Update Table

--- src/Class6.java
+++ src/Class6.java
@@ -14,13 +14,20 @@ public class Class6 {
 //        int a = 1;
 //        System.out.println(a);
 
-        for(int i = 0; i<= 10; i++){
-            if( i % 2 == 0){
-                System.out.println("Even " + i);
-            }
-            else{
-                System.out.println("Odd "+ i);
-            }
+//        for(int i = 0; i<= 10; i++){
+//            if( i % 2 == 0){
+//                System.out.println("Even " + i);
+//            }
+//            else{
+//                System.out.println("Odd "+ i);
+//            }
+//        }
+//        2*1 = 2
+//        2*2 = 4
+        System.out.println("Enter your table Number :  ");
+        int tableNumber = sc.nextInt();
+        for(int i = 1; i <=10; i++){
+            System.out.println( tableNumber +" * "+ i +" = "+  i*tableNumber);
         }
     }
 }
