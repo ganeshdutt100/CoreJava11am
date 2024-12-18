e17325f0121c1a4592a0448957001df989ba686e
unknown <gdutt2304@gmail.com>
2024-12-18 11:14:11 +0530

    Update Scanner

---
+++ src/Class6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Class6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your First Value :  ");
+        int a = sc.nextInt();
+        System.out.println("Enter your Second Value : ");
+        int b = sc.nextInt();
+                System.out.println(a+b);
+                sc.close();
+    }
+}
