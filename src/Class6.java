2c4b6c3443086fc4cb615908442aebc8461f535e
unknown <gdutt2304@gmail.com>
2024-12-18 11:50:08 +0530

    Update For loop

--- src/Class6.java
+++ src/Class6.java
@@ -3,11 +3,24 @@ import java.util.Scanner;
 public class Class6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your First Value :  ");
-        int a = sc.nextInt();
-        System.out.println("Enter your Second Value : ");
-        int b = sc.nextInt();
-                System.out.println(a+b);
-                sc.close();
+//        System.out.println("Enter your First Value :  ");
+//        int a = sc.nextInt();
+//        System.out.println("Enter your Second Value : ");
+//        int b = sc.nextInt();
+//                System.out.println(a+b);
+//                sc.close();
+
+
+//        int a = 1;
+//        System.out.println(a);
+
+        for(int i = 0; i<= 10; i++){
+            if( i % 2 == 0){
+                System.out.println("Even " + i);
+            }
+            else{
+                System.out.println("Odd "+ i);
+            }
+        }
     }
 }
