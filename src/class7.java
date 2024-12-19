0c327d64a7e01f2b6734ba37dd27bcef856ba9d6
unknown <gdutt2304@gmail.com>
2024-12-19 12:13:45 +0530

    Update pattern question s

--- src/class7.java
+++ src/class7.java
@@ -2,7 +2,10 @@ public class class7 {
     public static void main(String[] args) {
         int a = 5;
         for(int i  = 1;  i<= a;  i++){
-            for(int j =1; j<=i; j++){
+            for(int j =1; j<=a-i; j++){
+                System.out.print("   ");
+            }
+            for(int k = 1; k <= i; k++){
                 System.out.print(" * ");
             }
             System.out.println();
