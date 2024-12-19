cf6d7ff88ddd3a8df190e984f18546e6b9dba8e9
unknown <gdutt2304@gmail.com>
2024-12-19 12:03:59 +0530

    Update pattern

--- src/class7.java
+++ src/class7.java
@@ -1,9 +1,11 @@
 public class class7 {
     public static void main(String[] args) {
-        for(int i  = 1;  i<= 5;  i++){
-            for(int j =1; j<=3; j++){
-                System.out.println("i : " + i +  "J : "+ j);
+        int a = 5;
+        for(int i  = 1;  i<= a;  i++){
+            for(int j =1; j<=i; j++){
+                System.out.print(" * ");
             }
+            System.out.println();
 //            System.out.println(i);
         }
     }
