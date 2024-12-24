2581d9437a378a23b32ebb5a34ee8370cfa4e6de
unknown <gdutt2304@gmail.com>
2024-12-24 11:56:54 +0530

    Update Pattern Questions

--- src/class9.java
+++ src/class9.java
@@ -33,15 +33,48 @@ public class class9 {
 // ****
 //*****
 
-        for(int i = 5; i >= 1; i--){
-            for(int k = 1; k <= i; k++){
-                System.out.print(" ");
-            }
-            for(int j = i; j <= 5; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//        for(int i = 5; i >= 1; i--){
+//            for(int k = 1; k <= i; k++){
+//                System.out.print(" ");
+//            }
+//            for(int j = i; j <= 5; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
     }
 }
 //Program to Print Hourglass Pattern in Java
+//1)
+//         1
+//        2 3
+//       4 5 6
+//      7 8 9 10
+//    11 12 13 14 15
+
+//         1
+//        3 5
+//       7 9 11
+//     13 15 17 19
+
+//        *
+//        **
+//        ***
+//        ****
+//        *****
+//        ****
+//        ***
+//        **
+//        *
+
+//        *****
+//        ****
+//        ***
+//        **
+//        *
+//        *
+//        **
+//        ***
+//        ****
+//        *****
