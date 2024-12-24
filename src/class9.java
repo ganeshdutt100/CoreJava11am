c311d78f099f7febd142f64d53a9d39924e2ce94
unknown <gdutt2304@gmail.com>
2024-12-24 11:45:34 +0530

    Program to Print Hourglass Pattern in Java

--- src/class9.java
+++ src/class9.java
@@ -19,11 +19,26 @@ public class class9 {
 //            System.out.println();
 //        }
 
-  int number = 1;
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print( number+" " );
-                number++;
+//  int number = 1;
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print( number+" " );
+//                number++;
+//            }
+//            System.out.println();
+//        }
+//    *
+//   **
+//  ***
+// ****
+//*****
+
+        for(int i = 5; i >= 1; i--){
+            for(int k = 1; k <= i; k++){
+                System.out.print(" ");
+            }
+            for(int j = i; j <= 5; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
