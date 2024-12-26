5a13ede5518c5c6678a7c2de0f3cf8338bd97a9c
unknown <gdutt2304@gmail.com>
2024-12-26 12:11:06 +0530

    Update Array Question

--- src/class11.java
+++ src/class11.java
@@ -3,8 +3,18 @@ public class class11 {
 //        String str1 = "Ayush";
 //        String str2 = "Vardan";
 
-        String []arr = {"Ayush","Vardan"};
-        System.out.println(arr[0]+ " "+arr[1]);
+//        String []arr = {"Ayush","Vardan"};
+//        System.out.println(arr[0]+ " "+arr[1]);
+
+        int number[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+//        System.out.println(number[2]  );
+        int sum = 0;
+
+        for(int x : number) {
+            sum  = sum + x;
+//            System.out.println(x);
+        }
+        System.out.println(sum);
 
     }
 }
