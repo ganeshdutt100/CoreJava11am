ad72a844b623179c66713c94f05c7d1c70f56f20
unknown <gdutt2304@gmail.com>
2024-12-26 11:51:17 +0530

    Update Array

---
+++ src/class11.java
@@ -0,0 +1,10 @@
+public class class11 {
+    public static void main(String[] args) {
+//        String str1 = "Ayush";
+//        String str2 = "Vardan";
+
+        String []arr = {"Ayush","Vardan"};
+        System.out.println(arr[0]+ " "+arr[1]);
+
+    }
+}
