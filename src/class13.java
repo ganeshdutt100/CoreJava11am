59ede36b146cc3ce190ddfde0e81ceb83297fe47
unknown <gdutt2304@gmail.com>
2024-12-28 12:14:51 +0530

    Sum of array values

--- src/class13.java
+++ src/class13.java
@@ -1,16 +1,24 @@
 public class class13 {
     public static void main(String[] args) {
         int arr[] = {3,6,8,4,2};
-        int min = arr[0];
-        int max = arr[0];
-      for(int i=0; i<arr.length; i++) {
-          if(arr[i] < min){
-              min = arr[i]; //  min value
-          }
-          if(arr [i] >  max){
-              max = arr[i]; //  max value
-          }
-      }
-        System.out.println("min: " + min + " max: " + max);
+        int sum = 0 ;
+        for(int i = 0; i < arr.length; i++){
+             sum += arr[i];
+        }
+        System.out.println("sum = " + sum);
+
+
+
+//        int min = arr[0];
+//        int max = arr[0];
+//      for(int i=0; i<arr.length; i++) {
+//          if(arr[i] < min){
+//              min = arr[i]; //  min value
+//          }
+//          if(arr [i] >  max){
+//              max = arr[i]; //  max value
+//          }
+//      }
+//        System.out.println("min: " + min + " max: " + max);
     }
 }
