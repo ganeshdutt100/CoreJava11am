72d4bd64ac48381dd3e1a821c3470031982dcc02
unknown <gdutt2304@gmail.com>
2024-12-30 17:17:32 +0530

    Update Methods

--- src/class13.java
+++ src/class13.java
@@ -1,24 +1,17 @@
-public class class13 {
+public class class13{
     public static void main(String[] args) {
+
         int arr[] = {3,6,8,4,2};
-        int sum = 0 ;
-        for(int i = 0; i < arr.length; i++){
-             sum += arr[i];
-        }
-        System.out.println("sum = " + sum);
-
-
-
-//        int min = arr[0];
-//        int max = arr[0];
-//      for(int i=0; i<arr.length; i++) {
-//          if(arr[i] < min){
-//              min = arr[i]; //  min value
-//          }
-//          if(arr [i] >  max){
-//              max = arr[i]; //  max value
-//          }
-//      }
-//        System.out.println("min: " + min + " max: " + max);
+        int min = arr[0];
+        int max = arr[0];
+      for(int i=0; i<arr.length; i++) {
+          if(arr[i] < min){
+              min = arr[i]; //  min value
+          }
+          if(arr [i] >  max){
+              max = arr[i]; //  max value
+          }
+      }
+        System.out.println("min: " + min + " max: " + max);
     }
 }
---
+++ src/class14.java
@@ -0,0 +1,28 @@
+public class class14 {
+    public static void main(String[] args) {
+//        System.out.println("Hello world ");
+//        int a = 10;
+//        int b = 20;
+//        int c = a + b;
+//        System.out.println("Sum = " + c);
+//
+//        int d = 10;
+//        int e = 20;
+//        int f = d + e;
+//        System.out.println(f);
+
+        Method1();
+        System.out.println(Sum(12,67));
+        System.out.println(Sum(1,67));
+        System.out.println(Sum(32,67));
+        System.out.println(Sum(82,67));
+    }
+
+    public static void Method1(){
+        System.out.println("Inside Method1");
+
+    }
+    public static int Sum(int a ,int b){
+       return a + b;
+    }
+}
