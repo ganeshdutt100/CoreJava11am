e3d1ab339f9ac37c31d0a5e9929a51f5a7237ab4
Ganesh Dutt <gdutt2304@gmail.com>
2025-01-22 11:12:03 +0530

    OppsFirstQuestion

---
+++ src/oops/Questions/FirstQuestion.java
@@ -0,0 +1,46 @@
+package oops.Questions;
+ class Vehicle{
+     void start(){
+         System.out.println("Vehicle is starting....");
+     } void stop(){
+         System.out.println("Vehicle is stop!");
+     }
+ }
+ class Car extends Vehicle{
+  @Override
+    void start() {
+        System.out.println("Car starts");
+    }
+    void stop() {
+        System.out.println("Car stop");
+    }
+}
+class Bike extends Vehicle{
+  @Override
+    void start() {
+        System.out.println("Bike starts");
+    }
+    void stop() {
+        System.out.println("Bike stop");
+    }
+}
+class Truck extends Vehicle{
+  @Override
+    void start() {
+        System.out.println("Truck starts");
+    }
+    void stop() {
+        System.out.println("Truck stop");
+    }
+}
+public class FirstQuestion {
+    public static void main(String[] args) {
+//        Car obj1 = new Car();
+        // Create an array of vehicles and assign objects of Car, Bike, and Truck to it.
+        Vehicle[] vehicles = {new Car(), new Bike(), new Truck()};
+       for(Vehicle v : vehicles){
+           v.start();
+           v.stop();
+       }
+    }
+}
---
+++ src/oops/class5.java
@@ -0,0 +1,11 @@
+package oops;
+ class vehicle{
+     void start(){
+         System.out.println("Vehicle is starting....");
+     }
+ }
+public class class5 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
