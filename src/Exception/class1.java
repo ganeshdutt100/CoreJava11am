64d5a0b2d6e54038548ade6a6347a8be8eb12f8e
Ganesh Dutt <gdutt2304@gmail.com>
2025-01-31 12:21:55 +0530

    Exception using Try and Catch

--- src/Exception/class1.java
+++ src/Exception/class1.java
@@ -2,19 +2,31 @@ package Exception;
 
 public class class1 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
+//        int a = 10;
+//        int b = 0;
 //        int c  = a / b;
 //        System.out.println(c);
 //        System.out.println("Hello Ayush");
 
 
+//        try {
+//            int c = a / b;
+//            System.out.println(c);
+//        }
+//        catch (ArithmeticException e) {
+//            System.out.println("Arithmetic Exception: " + e.getMessage());
+//        }
+//        finally {
+//            System.out.println("This will always execute.");
+//        }
+
+
+        int number[] =  {1,2,3,4};
         try {
-            int c = a / b;
-            System.out.println(c);
+            System.out.println(number[4]);
         }
-        catch (ArithmeticException e) {
-            System.out.println("Arithmetic Exception: " + e.getMessage());
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception: " + e);
         }
         finally {
             System.out.println("This will always execute.");
