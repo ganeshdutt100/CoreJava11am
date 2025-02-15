ca64817f1eda9ea1015e1d5b7b052c598fb4bcfe
Ganesh Dutt <gdutt2304@gmail.com>
2025-02-15 11:57:35 +0530

    String Capital all First Letter

--- src/StringFile/code2.java
+++ src/StringFile/code2.java
@@ -3,7 +3,16 @@ package StringFile;
 public class code2 {
     public static void main(String[] args) {
         String str = "hello world";
-       String capitalFirstLetter =  str.substring(0,1).toUpperCase() + str.substring(1);
-        System.out.println(capitalFirstLetter);
+//       String capitalFirstLetter =  str.substring(0,1).toUpperCase() + str.substring(1);
+//        System.out.println(capitalFirstLetter);
+
+        String [] words = str.split(" ");
+
+        String result ="";
+//
+        for(String word : words){
+            result += word.substring(0,1).toUpperCase() + word.substring(1) + " ";
+        }
+        System.out.println(result);
     }
 }
